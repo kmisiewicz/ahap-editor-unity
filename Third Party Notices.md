3578b9e613dad1f3e1a4fb87bd628eccd1e5ada9
Krzysztof Misiewicz <k.misiewicz97@gmail.com>
2023-01-19 19:22:46 +0100

    added DSPLib and SpectralFluxAnalyzer

--- Third Party Notices.md
+++ Third Party Notices.md
@@ -1,21 +1,39 @@
 This package contains third-party software components governed by the license(s) indicated below:
 
 
-## ZXing.Net
+#### DSPLib
 
-License Type: Apache 2.0
+License Type: MIT
 
-Copyright © 2022 Michael Jahn
+Copyright © 2020 Steve Hageman
 
-https://github.com/micjahn/ZXing.Net
+https://www.codeproject.com/Articles/1107480/DSPLib-FFT-DFT-Fourier-Transform-Library-for-NET-6
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License at
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-    https://www.apache.org/licenses/LICENSE-2.0
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+---------
+#### SpectralFluxAnalyzer (slightly reformatted)
+ 
+License Type: MIT
+
+Copyright © 2018 Jesse Keogh
+
+https://github.com/jesse-scam/algorithmic-beat-mapping-unity
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ ThirdParty/SpectralFluxAnalyzer.cs
@@ -0,0 +1,117 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectralFluxInfo 
+{
+	public float Time;
+	public float SpectralFlux;
+	public float Threshold;
+	public float PrunedSpectralFlux;
+	public bool IsPeak;
+}
+
+public class SpectralFluxAnalyzer 
+{
+    public List<SpectralFluxInfo> SpectralFluxSamples;
+
+    // Sensitivity multiplier to scale the average threshold.
+    // In this case, if a rectified spectral flux sample is > 1.5 times the average, it is a peak
+    float _thresholdMultiplier = 1.5f;
+
+	int _sampleCount = 1024;
+	int _thresholdWindowSize = 50; // Number of samples to average in our window
+
+	float[] _currentSpectrum;
+	float[] _previousSpectrum;
+	int _indexToProcess;
+
+	public SpectralFluxAnalyzer (int sampleChunkSize, float sensitivity) 
+	{
+		_sampleCount = sampleChunkSize;
+		_thresholdMultiplier = 1f + sensitivity;
+		SpectralFluxSamples = new List<SpectralFluxInfo> ();
+
+		// Start processing from middle of first window and increment by 1 from there
+		_indexToProcess = _thresholdWindowSize / 2;
+
+		_currentSpectrum = new float[_sampleCount];
+		_previousSpectrum = new float[_sampleCount];
+	}
+
+	public int ThresholdWindowSize => _thresholdWindowSize;
+
+    public void SetCurrentSpectrum(float[] spectrum)
+    {
+        _currentSpectrum.CopyTo(_previousSpectrum, 0);
+        spectrum.CopyTo(_currentSpectrum, 0);
+    }
+
+    public void AnalyzeSpectrum(float[] spectrum, float time) 
+	{
+		// Set spectrum
+		SetCurrentSpectrum(spectrum);
+
+		// Get current spectral flux from spectrum
+		SpectralFluxInfo currentInfo = new();
+		currentInfo.Time = time;
+        currentInfo.SpectralFlux = CalculateRectifiedSpectralFlux();
+        SpectralFluxSamples.Add(currentInfo);
+
+        // We have enough samples to detect a peak
+        if (SpectralFluxSamples.Count >= _thresholdWindowSize)
+        {
+            // Get Flux threshold of time window surrounding index to process
+            SpectralFluxSamples[_indexToProcess].Threshold = GetFluxThreshold(_indexToProcess);
+
+            // Only keep amp amount above threshold to allow peak filtering
+            SpectralFluxSamples[_indexToProcess].PrunedSpectralFlux = GetPrunedSpectralFlux(_indexToProcess);
+
+            // After processing n samples, n-1 has neighbors (n-2, n) to determine peak
+            int indexToDetectPeak = _indexToProcess - 1;
+            if (IsPeak(indexToDetectPeak))
+                SpectralFluxSamples[indexToDetectPeak].IsPeak = true;
+
+            _indexToProcess++;
+		}
+	}
+
+    float CalculateRectifiedSpectralFlux()
+    {
+        float sum = 0f;
+        for (int i = 0; i < _sampleCount; i++)
+            sum += Mathf.Max(0f, _currentSpectrum[i] - _previousSpectrum[i]);
+        return sum;
+    }
+
+    float GetFluxThreshold(int spectralFluxIndex)
+    {
+        // How many samples in the past and future to include in average
+        int windowStartIndex = Mathf.Max(0, spectralFluxIndex - _thresholdWindowSize / 2);
+        int windowEndIndex = Mathf.Min(SpectralFluxSamples.Count - 1, spectralFluxIndex + _thresholdWindowSize / 2);
+
+        // Add up our spectral flux over the window
+        float sum = 0f;
+        for (int i = windowStartIndex; i < windowEndIndex; i++)
+            sum += SpectralFluxSamples[i].SpectralFlux;
+
+        // Return the average multiplied by sensitivity multiplier
+        float avg = sum / (windowEndIndex - windowStartIndex);
+        return avg * _thresholdMultiplier;
+    }
+
+    float GetPrunedSpectralFlux(int spectralFluxIndex)
+    {
+        return Mathf.Max(0f, SpectralFluxSamples[spectralFluxIndex].SpectralFlux - SpectralFluxSamples[spectralFluxIndex].Threshold);
+    }
+
+    bool IsPeak(int spectralFluxIndex)
+    {
+        if (SpectralFluxSamples[spectralFluxIndex].PrunedSpectralFlux > SpectralFluxSamples[spectralFluxIndex + 1].PrunedSpectralFlux &&
+            SpectralFluxSamples[spectralFluxIndex].PrunedSpectralFlux > SpectralFluxSamples[spectralFluxIndex - 1].PrunedSpectralFlux)
+        {
+            return true;
+        }
+        
+        return false;
+    }
+}
